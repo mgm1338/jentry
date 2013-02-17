05867db40caba1fa3cc6b70137e59cd6ac07f99f
Max <mgmiller@umich.edu>
2013-02-17 15:36:45 -0500

    Initial coding of heap, with tests, now need to debug

--- src/main/collections/heap/Heap_KeyTypeName_.java
+++ src/main/collections/heap/Heap_KeyTypeName_.java
@@ -1,6 +1,12 @@
 package collections.heap;
 
+import core.Const;
+import core.array.GrowthStrategy;
+import core.array.factory.ArrayFactoryInt;
+import core.array.factory.ArrayFactory_KeyTypeName_;
+import core.stub._key_;
 import core.util.comparator.Comparator;
+import core.util.comparator.Comparator_KeyTypeName_;
 
 /**
  * Copyright © 2012 Max Miller
@@ -8,9 +14,180 @@ import core.util.comparator.Comparator;
  */
 public class Heap_KeyTypeName_
 {
+    protected final ArrayFactoryInt intFactory;
+    protected final Comparator_KeyTypeName_ cmp;
+    protected final ArrayFactory_KeyTypeName_ keyFactory;
+    protected final GrowthStrategy growthStrategy;
 
-    public Heap_KeyTypeName_ (int initialCapacity, Comparator cmp)
+
+    private int size = 0;
+    protected _key_[] keys;
+    protected int[] tree;
+    protected int[] inverse;
+    protected int entryPtr = 0;
+    protected int[] freeList;
+    int freeListCt = 0;
+
+    public Heap_KeyTypeName_ (int initialCapacity, Comparator_KeyTypeName_ cmp)
     {
-        //To change body of created methods use File | Settings | File Templates.
+        this (initialCapacity, cmp, ArrayFactoryInt.defaultIntProvider,
+              ArrayFactory_KeyTypeName_.default_KeyTypeName_Provider,
+              GrowthStrategy.doubleGrowth);
+    }
+
+    public Heap_KeyTypeName_ (
+            int initialCapacity, Comparator_KeyTypeName_ cmp, ArrayFactoryInt intFactory,
+            ArrayFactory_KeyTypeName_ keyFactory, GrowthStrategy growthStrategy)
+    {
+        this.cmp = cmp;
+        this.intFactory = intFactory;
+        this.keyFactory = keyFactory;
+        this.growthStrategy = growthStrategy;
+        tree = intFactory.alloc (initialCapacity, Const.NO_ENTRY);
+        inverse = intFactory.alloc (initialCapacity);
+        keys = keyFactory.alloc (initialCapacity);
+        freeList = intFactory.alloc (4);
+    }
+
+    public int getSize ()
+    {
+        return size;
+    }
+
+    public boolean isEmpty ()
+    {
+        return size == 0;
+    }
+
+    public void removeTop ()
+    {
+        remove (tree[1]);
+    }
+
+    public _key_ peek ()
+    {
+        return keys[tree[1]];
+    }
+
+    public int insert (_key_ key)
+    {
+        int entry = getNextEntry ();
+        keys[entry] = key;
+        int treeSpot = getNextFreeTreeSpot ();
+        inverse[entry] = treeSpot;
+        shiftUp (key, treeSpot);
+        size++;
+        return entry;
+    }
+
+    public boolean remove (int entry)
+    {
+        int spot = inverse[entry];
+        if (entry == Const.NO_ENTRY)
+        {
+            return false;
+        }
+        else
+        {
+            shiftDown (spot);
+        }
+        size--;
+        return true;
+    }
+
+    protected void shiftDown (int curSpot)
+    {
+        int left = leftChild (curSpot);
+        int right = rightChild (curSpot);
+        while (left != Const.NO_ENTRY && right != Const.NO_ENTRY)
+        {
+            if (left == Const.NO_ENTRY)
+            {
+                flip (curSpot, left);
+            }
+            else if (right == Const.NO_ENTRY)
+            {
+                flip (curSpot, right);
+            }
+            else
+            {
+                int cmpResult = cmp.compare (keys[left], keys[right]);
+                if (cmpResult <= 0)
+                {
+                    flip (curSpot, left);
+                }
+                else
+                {
+                    flip (curSpot, right);
+                }
+            }
+            left = leftChild (curSpot);
+            right = rightChild (curSpot);
+        }
+    }
+
+    protected void shiftUp (_key_ key, int curSpot)
+    {
+        int parent = parent (curSpot);
+        while (cmp.compare (key, keys[tree[parent]]) <= 0 && curSpot != 1)
+        {
+            flip (curSpot, parent);
+            curSpot = parent;
+            parent = parent (parent);
+        }
+    }
+
+    protected int getNextFreeTreeSpot ()
+    {
+        int spot = 1;
+        while ((spot * 2 < size))
+        {
+            spot *= 2;
+        }
+        //add another level
+        tree = intFactory.ensureArrayCapacity
+                (tree, spot * 2, Const.NO_ENTRY, growthStrategy);
+        inverse = intFactory.ensureArrayCapacity (inverse, spot * 2, growthStrategy);
+        while (tree[spot++] != Const.NO_ENTRY)
+        {
+        }
+        return spot;
+    }
+
+    protected int getNextEntry ()
+    {
+        if (freeListCt != 0)
+        {
+            return freeList[--freeListCt];
+        }
+        int nextEntry = entryPtr++;
+        keys = keyFactory.ensureArrayCapacity (keys, nextEntry + 1,
+                                               growthStrategy);
+        return nextEntry;
+    }
+
+    protected void flip (int a, int b)
+    {
+        int temp = tree[b];
+        tree[b] = tree[a];
+        tree[a] = temp;
+        temp = inverse[b];
+        inverse[a] = inverse[b];
+        inverse[b] = temp;
+    }
+
+    protected int leftChild (int parent)
+    {
+        return parent * 2;
+    }
+
+    protected int rightChild (int parent)
+    {
+        return (parent * 2) + 1;
+    }
+
+    protected int parent (int child)
+    {
+        return (child / 2);
     }
 }
--- src/test/collections/heap/TestHeap_KeyTypeName_.java
+++ src/test/collections/heap/TestHeap_KeyTypeName_.java
@@ -1,9 +1,16 @@
 package collections.heap;
 
+import core.stub.IntValueConverter;
+import core.stub._key_;
 import core.util.comparator.Comparator_KeyTypeName_;
 import core.util.comparator.Comparators;
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+import util.TestUtils_KeyTypeName_;
+
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Copyright © 2012 Max Miller
@@ -12,25 +19,151 @@ import org.junit.Test;
 public class TestHeap_KeyTypeName_
 {
 
+    private static final int TEST_SIZE = 5;
     Heap_KeyTypeName_ heap;
 
     @Before
-    public void setup()
+    public void setup ()
     {
         heap = new Heap_KeyTypeName_ (8, new Comparators._KeyTypeName_Asc ());
     }
 
-    //Start insert tests
     @Test
-    public void testInsertCompact()
+    public void testInsertCompact ()
     {
+        TestCase.assertTrue (heap.isEmpty ());
+        TestCase.assertEquals (0, heap.getSize ());
+
+        for (int i = 0; i < TEST_SIZE; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (i)));
+        }
+        TestCase.assertEquals (TEST_SIZE, heap.getSize ());
+
+
+        heap.remove (3);
+        TestCase.assertEquals (3,
+                               heap.insert (IntValueConverter._key_FromInt (3)));
+        TestCase.assertEquals (TEST_SIZE, heap.getSize ());
 
     }
 
-
-    protected void assertInOrder(Heap_KeyTypeName_ heap)
+    @Test
+    public void testReverseInsertionCorrectOrder ()
     {
+        for (int i = 0; i < TEST_SIZE; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (TEST_SIZE - i)));
+        }
+        assertInAscOrder (heap);
+    }
 
+    @Test
+    public void testRandomInsertionCorrectOrder ()
+    {
+        Random random = new Random (42);
+        for (int i = 0; i < TEST_SIZE; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (random.nextInt (16))));
+        }
+    }
+
+    @Test
+    public void testBackwardsWithDoubleGrowth ()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (TEST_SIZE - i)));
+        }
+        TestCase.assertEquals (heap.keys.length, 32);
+        TestCase.assertEquals (heap.tree.length, 32);
+        TestCase.assertEquals (heap.inverse.length, 32);
+        TestCase.assertEquals (24, heap.getSize ());
+        assertInAscOrder (heap);
+    }
+
+    @Test
+    public void testRandomWithDoubleGrowth ()
+    {
+        Random random = new Random (42);
+        for (int i = 0; i < 24; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (random.nextInt (16))));
+        }
+        TestCase.assertEquals (heap.keys.length, 32);
+        TestCase.assertEquals (heap.tree.length, 32);
+        TestCase.assertEquals (heap.inverse.length, 32);
+        TestCase.assertEquals (24, heap.getSize ());
+        assertInAscOrder (heap);
+    }
+
+    @Test
+    public void manyWithSameOneHigher ()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (4)));
+        }
+
+        heap.insert (IntValueConverter._key_FromInt (5));
+        TestCase.assertEquals (IntValueConverter._key_FromInt (5), heap.peek ());
+        TestCase.assertEquals (25, heap.getSize ());
+    }
+
+    @Test
+    public void manyWithSameOneLower ()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            TestCase.assertEquals (i,
+                                   heap.insert (IntValueConverter._key_FromInt (4)));
+        }
+
+        heap.insert (IntValueConverter._key_FromInt (3));
+
+        for (int i = 0; i < 24; i++)
+        {
+            heap.removeTop ();
+        }
+        TestCase.assertEquals (IntValueConverter._key_FromInt (3), heap.peek ());
+        TestCase.assertEquals (1, heap.getSize ());
+    }
+
+    @Test
+    public void fullRemoval ()
+    {
+        testRandomWithDoubleGrowth ();
+        while (!heap.isEmpty ())
+        {
+            heap.removeTop ();
+            assertInAscOrder (heap);
+        }
+        TestCase.assertTrue (heap.isEmpty ());
+        TestCase.assertEquals (0, heap.getSize ());
+    }
+
+    //test free list, and internal equals with copy methods
+
+
+    protected void assertInAscOrder (Heap_KeyTypeName_ heap)
+    {
+        _key_[] collected = new _key_[heap.getSize ()];
+        int ct = 0;
+        while (!heap.isEmpty ())
+        {
+            collected[ct++] = heap.peek ();
+            heap.removeTop ();
+        }
+        _key_[] copy = new _key_[collected.length];
+        System.arraycopy (collected, 0, copy, 0, collected.length);
+        Arrays.sort (copy);
+        TestUtils_KeyTypeName_.assertArrayContentsEqual (collected, copy);
     }
 
 }
