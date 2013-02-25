33c6c2697b47a92575e17e021f5594643e312d3a
Max <mgmiller@umich.edu>
2013-02-25 10:52:42 -0500

    finishing up local commit to merge in remote

--- src/main/collections/relationship/ManyToManyInt.java
+++ src/main/collections/relationship/ManyToManyInt.java
@@ -1,5 +1,9 @@
 package collections.relationship;
 
+import core.Const;
+import core.array.GrowthStrategy;
+import core.array.factory.ArrayFactoryInt;
+
 /**
  * Copyright 2/8/13
  * All rights reserved.
@@ -7,6 +11,74 @@ package collections.relationship;
  * User: Max Miller
  * Created: 2/8/13
  */
-public class ManyToManyInt
+public class ManyToManyInt extends OneToManyInt
 {
+
+    protected boolean countRights;
+    protected int[] rightCounts = null;
+
+    /** Array that will hold the first relationship from the index, will be the handle into <b>associations</b> */
+    protected int[] rights;
+    /** The next pointer that will fill the index of the current entry to the next handle (see class docs) */
+    protected int[] rightNexts;
+
+
+    /**
+     * Constructor
+     *
+     * @param initialLefts        the initial number of lefts that we can hold (note that is it compact, so inserting
+     *                            above this number will immediately cause a growth)
+     * @param initialRights       the initial number of rights that we can hold (note that is it compact, so inserting
+     *                            above this number will immediately cause a growth)
+     * @param initialAssociations the size of initial number of associations we can make.
+     * @param countLefts          whether or not to track the counts of our associations per left.
+     * @param countRights         whether or not to track the counts of our associations per right.
+     */
+    public ManyToManyInt( int initialLefts, int initialRights, int initialAssociations, boolean countLefts, boolean countRights )
+    {
+        this( initialLefts, initialRights, initialAssociations, countLefts, countRights, GrowthStrategy.doubleGrowth,
+              ArrayFactoryInt.defaultIntProvider );
+    }
+
+    /**
+     * Full Constructor
+     *
+     * @param initialLefts        the initial number of lefts that we can hold (note that is it compact, so inserting
+     *                            above this number will immediately cause a growth)
+     * @param initialRights       the initial number of rights that we can hold (note that is it compact, so inserting
+     *                            above this number will immediately cause a growth)
+     * @param initialAssociations the size of initial number of associations we can make.
+     * @param countLefts          whether or not to track the counts of our associations per left.
+     * @param countRights         whether or not to track the counts of our associations per right.
+     * @param growthStrategy      the growth strategy for growing our lefts array, associations array,
+     *                            and counts array (if counting the associations)
+     * @param intFactory          factory used to grow/allocate the arrays
+     */
+    public ManyToManyInt( int initialLefts, int initialRights, int initialAssociations, boolean countLefts,
+                          boolean countRights, GrowthStrategy growthStrategy, ArrayFactoryInt intFactory )
+    {
+        super( initialLefts, initialAssociations, countLefts, growthStrategy, intFactory );
+        this.countRights = countRights;
+        if( countRights )
+        {
+            rightCounts = intFactory.alloc( initialRights );
+        }
+        rights = intFactory.alloc( initialRights, Const.NO_ENTRY );
+        rightNexts = intFactory.alloc( initialAssociations, Const.NO_ENTRY );
+    }
+
+    public int getCountForRight( int right )
+    {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public int getNextLeftEntry( int right, int prevEntry)
+    {
+        return 0;
+    }
+
+    public int[] getAllLeftAssociations( int right, int[] target, int mark )
+    {
+        return new int[ 0 ];  //To change body of created methods use File | Settings | File Templates.
+    }
 }
--- src/test/collections/relationship/TestManyToManyInt.java
+++ src/test/collections/relationship/TestManyToManyInt.java
@@ -1,5 +1,11 @@
 package collections.relationship;
 
+import core.Const;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import util.TestUtilsInt;
+
 /**
  * Copyright 2/9/13
  * All rights reserved.
@@ -9,4 +15,94 @@ package collections.relationship;
  */
 public class TestManyToManyInt
 {
+    ManyToManyInt manyToManyNoCounts;
+    ManyToManyInt manyToManyCounts;
+
+
+    @Before
+    public void setup()
+    {
+        manyToManyNoCounts = new ManyToManyInt( 8, 8, 8, false, false );
+        manyToManyCounts = new ManyToManyInt( 8, 8, 8, true, true );
+
+        TestCase.assertEquals( 0, manyToManyCounts.getSize() );
+        TestCase.assertEquals( 0, manyToManyNoCounts.getSize() );
+
+        manyToManyCounts.associate( 0, 1 );
+        manyToManyCounts.associate( 2, 1 );
+        manyToManyCounts.associate( 3, 1 );
+        manyToManyCounts.associate( 4, 1 );
+        TestCase.assertEquals( 4, manyToManyCounts.getCountForRight( 1 ) );
+        manyToManyCounts.associate( 3, 19 );
+        manyToManyCounts.associate( 3, 20 );
+        TestCase.assertEquals( 1, manyToManyCounts.getCountForRight( 20 ) );
+        manyToManyCounts.associate( 3, 21 );
+        manyToManyCounts.associate( 25, 6 );
+        manyToManyCounts.associate( 26, 6 );
+        manyToManyCounts.associate( 27, 6 );
+        TestCase.assertEquals( 3, manyToManyCounts.getCountForRight( 3 ) );
+
+
+
+        manyToManyNoCounts.associate( 0, 1 );
+        manyToManyNoCounts.associate( 2, 1 );
+        manyToManyNoCounts.associate( 3, 1 );
+        manyToManyNoCounts.associate( 4, 1 );
+        TestCase.assertEquals( 4, manyToManyNoCounts.getCountForRight( 1 ) );
+        manyToManyNoCounts.associate( 3, 19 );
+        manyToManyNoCounts.associate( 3, 20 );
+        TestCase.assertEquals( 1, manyToManyNoCounts.getCountForRight( 20 ) );
+        manyToManyNoCounts.associate( 3, 21 );
+        manyToManyNoCounts.associate( 25, 6 );
+        manyToManyNoCounts.associate( 26, 6 );
+        manyToManyNoCounts.associate( 27, 6 );
+        TestCase.assertEquals( 3, manyToManyNoCounts.getCountForRight( 3 ) );
+
+
+        TestCase.assertEquals( 10, manyToManyCounts.getSize() );
+        TestCase.assertEquals( 10, manyToManyNoCounts.getSize() );
+    }
+
+
+    @Test
+    public void iterateLeftsForRight()
+    {
+
+        int entry = Const.NO_ENTRY;
+        int[] rights = new int[ 4 ];
+        int rtCt = 0;
+        while( ( entry = manyToManyNoCounts.getNextLeftEntry( 1, entry ) ) != Const.NO_ENTRY )
+        {
+            int right = manyToManyNoCounts.getRight( entry );
+            rights[ rtCt++ ] = right;
+        }
+        TestCase.assertEquals( rtCt, 4 );
+        //insert order
+        TestCase.assertEquals(  0, rights[ 0 ] );
+        TestCase.assertEquals(  2, rights[ 1 ] );
+        TestCase.assertEquals(  3, rights[ 2 ] );
+        TestCase.assertEquals(  4, rights[ 2 ] );
+
+
+        TestUtilsInt.assertArrayContentsToLen( rights, manyToManyNoCounts.getAllLeftAssociations( 2, null,
+                                                                                             Const.NO_ENTRY ), 3 );
+    }
+
+
+    /** Assert that we will grow an array that is one off holding the set of associations */
+    @Test
+    public void getIterationsWithDifferentTargets()
+    {
+        int[] ofByOne = new int[ 3 ];
+        //assert that we will grow the ofByOne array, we already know null will get correct array from test above
+        TestUtilsInt.assertArrayContentsToLen( manyToManyNoCounts.getAllLeftAssociations( 1, null, Const.NO_ENTRY ),
+                                               manyToManyNoCounts.getAllLeftAssociations( 1, ofByOne,
+                                                                                          Const.NO_ENTRY ), 4 );
+
+    }
+
+
+
+
+
 }
